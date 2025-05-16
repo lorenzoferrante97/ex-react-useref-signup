98d6951e490b1c01482848502b8d67e6f843a73c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-16 17:41:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// start code
+
+import { useGlobalContext } from './context/GlobalContext';
+
+function App() {
+  return (
+    <>
+      <main id="main"></main>
+    </>
+  );
+}
+
+export default App;
